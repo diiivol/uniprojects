47100de2dc39084da009802c41890b4e57843b18
Daria <146133615+diiivol@users.noreply.github.com>
2023-12-14 14:03:26 +0000

    modified:   contacts.html 	modified:   css/style.css 	renamed:    img/logo.svg -> img/logo_black.svg 	modified:   js/script.js 	modified:   menu.html

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+function toggleText(id) {
+  var element = document.getElementById(id);
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+    element.style.opacity = '0';
+    var height = element.clientHeight + 'px';
+    element.style.height = '0';
+    setTimeout(function() {
+      element.style.transition = 'height 0.3s ease, opacity 0.3s ease';
+      element.style.height = height;
+      element.style.opacity = '1';
+    }, 0);
+    setTimeout(function() {
+      element.style.height = 'auto'; // Set height to auto after transition
+    }, 300);
+  } else {
+    element.style.height = element.clientHeight + 'px';
+    element.style.opacity = '1';
+    setTimeout(function() {
+      element.style.transition = 'height 0.3s ease, opacity 0.3s ease';
+      element.style.height = '0';
+      element.style.opacity = '0';
+    }, 0);
+    setTimeout(function() {
+      element.style.display = 'none';
+      element.style.transition = '';
+      element.style.height = 'auto'; // Set height to auto after transition
+    }, 300);
+  }
+}
